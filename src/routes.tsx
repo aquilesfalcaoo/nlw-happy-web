725fa7d1ba38356a7e55ca8ec9b3dbb873ef41da
Aquiles Falcão <aquilesfalcao@hotmail.com>
2020-10-13 11:58:38 -0300

    last commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {HashRouter, Switch, Route} from 'react-router-dom';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
 
 function Routes() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanagesMap} />
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
